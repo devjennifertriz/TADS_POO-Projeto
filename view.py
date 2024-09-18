47df18e01cd17b8248467240a7562e2cadf6fe45
Gabrielle <143741502+gabifpp0@users.noreply.github.com>
2024-09-18 16:35:02 +0000

    Adicionando funções de Pedido e Produto

--- view.py
+++ view.py
@@ -1,4 +1,7 @@
 from models.Usuario import Usuario, Usuarios
+from models.pedido import Pedido, Pedidos
+from models.produto import Produto, Produtos
+from models.categoria import Categoria, Categorias
 
 class View:
     @staticmethod
@@ -8,10 +11,49 @@ class View:
     @staticmethod
     def Usuario_Listar():
         return Usuarios.Listar()
+    def Usuario_ListarId(id):
+        return Usuarios.Listar_id(id)
     @staticmethod
     def Usuario_Atualizar(id, nome, email, fone, senha):
         Atualizando = Usuario(id, nome, email, fone, senha)
+        Usuarios.Atualizar(Atualizando)
     @staticmethod
     def Usuario_Excluir(id):
         Excluindo = Usuario(id, "semnome", "sememail", "")
         Usuarios.Excluir(Excluindo)
+    @staticmethod
+    def Usuario_Admin():
+        pass
+    def Usuario_Login(email, senha):
+        return True
+    @staticmethod
+    def Pedido_Inserir(enderecoEntrega, status, valorTotal, qtdProduto, idProduto, idUsuario):
+        Inserindo = Pedido(0, enderecoEntrega, status, valorTotal, qtdProduto, idProduto, idUsuario)
+        Pedidos.Inserir(Inserindo)
+    @staticmethod
+    def Pedido_Listar():
+        return Pedidos.Listar()
+    @staticmethod
+    def Pedido_Atualizar(id, enderecoEntrega, status, valorTotal, qtdProduto, idProduto, idUsuario):
+        Atualizando = Pedido(id, enderecoEntrega, status, valorTotal, qtdProduto, idProduto, idUsuario)
+        Pedidos.Atualizar(Atualizando)
+    @staticmethod
+    def Pedido_Excluir(id):
+        Excluindo = Pedido(id, "", "", "", "", "", "")
+        Pedidos.Excluir(Excluindo)
+    @staticmethod
+    def Produto_Inserir(nome, valor, categoria):
+        Inserindo = Produto(0, nome, valor, categoria)
+        Produtos.Inserir(Inserindo)
+    @staticmethod
+    def Produto_Listar():
+        return Produtos.Listar()
+    @staticmethod
+    def Produto_Atualizar(id, nome, valor, categoria):
+        Atualizando = Produto(id, nome, valor, categoria)
+        Produtos.Atualizar(Atualizando)
+    @staticmethod
+    def Produto_Excluir(id):
+        Excluindo = Produto(id, "", "", "")
+        Produtos.Excluir(Excluindo)
+    
