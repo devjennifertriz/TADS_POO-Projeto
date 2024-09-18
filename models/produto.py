f87bd4c112219e5ce805c30b9f89f02855b23361
Gabrielle <143741502+gabifpp0@users.noreply.github.com>
2024-09-18 13:15:42 +0000

    Classe produto

---
+++ models/produto.py
@@ -0,0 +1,48 @@
+import json
+from models.modelo import Modelo
+
+class Produto:
+    def __init__(self, id: int, nome: str, valor: float, categoria: str, qtdEstoque: int):
+        self.__id = id
+        self.__nome = nome
+        self.__valor = valor
+        self.__categoria = categoria
+        self.__qtdEstoque = qtdEstoque
+    def __str__(self):
+        return f'ID - {self.__id} Nome - {self.__nome} Valor - {self.__valor} Categoria - {self.__categoria} Estoque - {self.__qtdEstoque}'
+
+class Produtos(Modelo):
+
+    @staticmethod
+    def Abrir(cls, obj):
+        cls.objetos = []
+        with open("produtos.json", mode='r') as arquivo:
+            produtos_json = json.load(arquivo)
+            for obj in produtos_json:
+                pr = Produto(obj['"Id'], obj['"nome'], obj['"valor'], obj['"categoria'], obj['"qtdEstoque'])
+        cls.objetos.append(pr)
+
+    @staticmethod
+    def Salvar(cls):
+        with open("produtos.json", mode='w') as arquivo:
+            json.dump(cls.objetos, arquivo, default=vars)
+        
+    @staticmethod
+    def Inserir(cls, obj):
+        return super().Inserir(cls, obj)
+
+    @staticmethod
+    def Listar(cls):
+        return super().Listar(cls)
+
+    @staticmethod
+    def Listar_id(cls):
+        return super().Listar_id(cls, id)
+
+    @staticmethod
+    def Excluir(cls, obj):
+        return super().Excluir(cls, obj)
+
+    @staticmethod
+    def Atualizar(cls, obj):
+        return super().Atualizar(cls, obj)
