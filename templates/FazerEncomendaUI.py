5c633920f717979f5b54871ab4a46bf928e9fa6b
Gabrielle <143741502+gabifpp0@users.noreply.github.com>
2024-09-19 17:19:02 +0000

    Corrigindo

--- templates/FazerEncomendaUI.py
+++ templates/FazerEncomendaUI.py
@@ -10,34 +10,22 @@ class FazerEncomendaUI:
         
         #atributos encomenda
 
-        #id = st.number_input("Id da encomenda", min_value=0)
         st.text("Nossos produtos:")
-        itens = View.Item_Listar()
+        produtos = View.Produto_Listar()
+        st.table(produtos)
 
-        item = st.selectbox("", itens)
         endereco = st.text_input("Endereço de Entrega")
         status = "Pendente"
-        valorT = st.number_input("Valor total", min_value=0.0)
-        #idusuario = st.number_input("Id do usuário", min_value=0)
+        valorT = 0
         idusuario = 0
 
-        #item = View.Item_Listar()
-        #op = st.selectbox("", item)
-        #atributos item
         #idi = st.number_input("Id do Item", min_value=0)
-        quantidade = st.text_input("Quantidade")
-        valor = st.number_input("Valor do produto")
-        idencomenda = 0
-        idproduto = st.multiselect(item)
         
         if st.button("Pedir"):
             status = "Pedido feito"
-            View.Item_Inserir(quantidade, valor, idencomenda, idproduto)
             View.Encomenda_Inserir(endereco, status, valorT, idusuario)
             
             st.success("Pedido feito com sucesso!")
             print("O número do seu pedido é:")
 
             
-            #item = View.Item_Listar()
-            #op = st.selectbox("", item)
