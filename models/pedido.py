15198cc80d4d6ee6f6da3e19942ce374c5f93d17
Gabrielle <143741502+gabifpp0@users.noreply.github.com>
2024-09-19 00:27:29 +0000

    Adicionando Gets e Sets à Pedido

--- models/pedido.py
+++ models/pedido.py
@@ -2,17 +2,34 @@ import json
 from models.modelo import Modelo
 
 class Pedido:
-    def __init__(self, id: int, endereçoEntrega: str, status: str, valorTotal: int, qtdProduto: int, idProduto: int, idUsuario: int):
+    def __init__(self, id: int, endereçoEntrega: str, status: str, valorTotal: int, idUsuario: int):
         self.__id = id
         self.__endereco = endereçoEntrega
         self.__status = status
         self.__valorTotal = valorTotal
-        self.__qtdProduto = qtdProduto
-        self.__idProduto = idProduto
         self.__idUsuario = idUsuario
     def __str__(self):
-        return f'ID - {self.__id} Endereço - {self.__endereco} Status - {self.__status} Valor Total - {self.__valorTotal} Quantidade - {self.__qtdProduto} IdProduto{self.__idProduto} IdUsuario - {self.__idUsuario}'
-    
+        return f'ID - {self.__id} Endereço - {self.__endereco} Status - {self.__status} Valor Total - {self.__valorTotal} IdUsuario - {self.__idUsuario}'
+    def set_id(self, id):
+        if id < 0: raise ValueError()
+    def set_endereco(self, enderecoEntrega):
+        if enderecoEntrega == "": raise ValueError()
+    def set_status(self, status):
+        if status == "": raise ValueError()
+    def set_valor(self, valorTotal):
+        if valorTotal < 0: raise ValueError()
+    def set_idUsuario(self, idUsuario):
+        if idUsuario == "": raise ValueError()
+    def get_id(self):
+        return self.__id
+    def get_enderco(self):
+        return self.__endereco
+    def get_status(self):
+        return self.__status
+    def get_valorTotaç(self):
+        return self.__valorTotal
+    def get_idUsuario(self):
+        return self.__idUsuario
 class Pedidos(Modelo):
 
     @staticmethod
